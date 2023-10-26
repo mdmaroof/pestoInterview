bca7e3f068629080f6ac9d660c675c0156aa61dc
Mohd Maroof <mdmaroof@outlook.com>
2023-10-26 10:59:07 +0530

    status and descripption

--- backend/model/todo.js
+++ backend/model/todo.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
 const todoSchema = mongoose.Schema(
     {
         title: { type: String, required: true },
+        description: { type: String, required: true },
+        status: { type: String, required: true },
     },
     {
-        timestamps: true,
         collection: "todo"
     },
 )
